cac2c4e1af4b8a1e52cdf3a35172846395282917
estercen <ecentella2140@esdmadrid.es>
2019-03-15 18:00:48 +0100

    cambios

--- code.js
+++ code.js
@@ -4,18 +4,21 @@ $(function(){
 
 $("#paja").draggable({
   /*stop: function(event){$(this).html("¡BIEN HECHO!")},*/
-  cursor: "move"
+  cursor: "move",
+  revert: "invalid"
+
 
   });
     $("#casa-paja").droppable({
-      drop: function(event){$(this).fadeTo(500, 0.4)},
+      drop: function(event){$(this).fadeTo(500, 1)},
       tolerance: "pointer" 
   });
     $("#casa-paja").droppable("option", "accept", "#paja"); 
 
  $("#madera").draggable({
  /* stop: function(event){$(this).html("¡BIEN HECHO!")},*/
-  cursor: "move"
+  cursor: "move",
+  revert: "invalid"
   });
     $("#casa-madera").droppable({
       drop: function(event){$(this).fadeTo(500, 0.4)},
@@ -25,11 +28,14 @@ $("#paja").draggable({
     
    $("#ladrillo").draggable({
   /*stop: function(event){$(this).html("¡BIEN HECHO!")},*/
-  cursor: "move"
+  cursor: "move",
+  revert: "invalid"
   });
     $("#casa-ladrillo").droppable({
      drop: function(event){$(this).fadeTo(500, 0.4)},
       tolerance: "pointer"
     });
      $("#casa-ladrillo").droppable("option", "accept", "#ladrillo");
+
+
 });
