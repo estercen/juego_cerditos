814ad9dff866575f8b03bb3352d88207c95c3887
estercen <ecentella2140@esdmadrid.es>
2019-03-28 20:56:43 +0100

    ultima-version

--- code.js
+++ code.js
@@ -10,8 +10,9 @@ $("#paja").draggable({
 
   });
     $("#casa-paja").droppable({
-      drop: function(event){$(this).fadeTo(500, 1)},
-      tolerance: "pointer" 
+      drop: function(event){$(this).fadeTo(500, 0.4)},
+      tolerance: "pointer"
+
   });
     $("#casa-paja").droppable("option", "accept", "#paja"); 
 
@@ -37,5 +38,137 @@ $("#paja").draggable({
     });
      $("#casa-ladrillo").droppable("option", "accept", "#ladrillo");
 
+   /*$(function newDoc() {
+      window.location.assign("index.html")
+    });
+  
+/*LABERINTO*/
+$("#pieza1").click(function(){
+    $("#pieza1").animate(
+    { deg: 90 },
+    {
+      duration: 300,
+      step: function(go) {
+        $(this).css({ transform: 'rotate(' + go + 'deg)' });
+      }
+    }
+  );
+  });
+
+  $("#pieza2").click(function(){
+    $("#pieza2").animate(
+    { deg: 90 },
+    {
+      duration: 300,
+      step: function(go) {
+        $(this).css({ transform: 'rotate(' + go + 'deg)' });
+      }
+    }
+  );
+  });
+
+  $("#pieza3").click(function(){
+    $("#pieza3").animate(
+    { deg: 90 },
+    {
+      duration: 300,
+      step: function(go) {
+        $(this).css({ transform: 'rotate(' + go + 'deg)' });
+      }
+    }
+  );
+  });
+
+  $("#pieza4").click(function(){
+    $("#pieza4").animate(
+    { deg: 90 },
+    {
+      duration: 300,
+      step: function(go) {
+        $(this).css({ transform: 'rotate(' + go + 'deg)' });
+      }
+    }
+  );
+  });
+
+$("#pieza4").click(function(){
+    $("#pieza4").animate(
+    { deg: 90 },
+    {
+      duration: 300,
+      step: function(go) {
+        $(this).css({ transform: 'rotate(' + go + 'deg)' });
+      }
+    }
+  );
+  });
+
+$("#pieza5").click(function(){
+    $("#pieza5").animate(
+    { deg: 90 },
+    {
+      duration: 300,
+      step: function(go) {
+        $(this).css({ transform: 'rotate(' + go + 'deg)' });
+      }
+    }
+  );
+  });
+
+$("#pieza6").click(function(){
+    $("#pieza6").animate(
+    { deg: 90 },
+    {
+      duration: 300,
+      step: function(go) {
+        $(this).css({ transform: 'rotate(' + go + 'deg)' });
+      }
+    }
+  );
+  });
+
+$("#pieza7").click(function(){
+    $("#pieza7").animate(
+    { deg: 90 },
+    {
+      duration: 300,
+      step: function(go) {
+        $(this).css({ transform: 'rotate(' + go + 'deg)' });
+      }
+    }
+  );
+  });
+
+$("#pieza8").click(function(){
+    $("#pieza8").animate(
+    { deg: 90 },
+    {
+      duration: 300,
+      step: function(go) {
+        $(this).css({ transform: 'rotate(' + go + 'deg)' });
+      }
+    }
+  );
+  });
+
+$("#pieza9").click(function(){
+    $("#pieza9").animate(
+    { deg: 90 },
+    {
+      duration: 300,
+      step: function(go) {
+        $(this).css({ transform: 'rotate(' + go + 'deg)' });
+      }
+    }
+  );
+      /*alert('¡BIEN HECHO!' );*/
+
+  });
+
+/*complete: function(){ alert('done') }
+  $(function newDoc() {
+      window.location.assign("index.html")
+    });*/
 
 });
+
